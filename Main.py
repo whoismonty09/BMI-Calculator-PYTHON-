936be2c8bfa71ebdb9946f7a907035226a8a12c7
Mayank malviya <mayankmalviya440@gmail.com>
2026-02-27 01:07:57 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,16 @@
+print("BMI Calculator developed by Monty")
+weight = float(input("Enter your weight in kg: "))
+height = float(input("Enter your height in meters: "))
 
+bmi = weight / (height * height)
+
+print("Your BMI is:", round(bmi, 2))
+
+if bmi < 18.5:
+    print("Underweight")
+elif bmi < 25:
+    print("Normal weight")
+elif bmi < 30:
+    print("Overweight")
+else:
+    print("Obese")
